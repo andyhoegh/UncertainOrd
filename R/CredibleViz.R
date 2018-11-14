4d99f3852d6208f195f8e248980214f85d13327a
Andy Hoegh <andrew.hoegh@montana.edu>
2018-11-14 16:35:37 -0700

    Updated viz function

--- R/CredibleViz.R
+++ R/CredibleViz.R
@@ -1,37 +1,60 @@
-#' Count Data Simulation
+#' Visualize Uncertainty in Projection
 #'
 #' This function simulates multivariate Poisson count data from a latent factor model.
-#' @param n - number of sites.
-#' @param p - number of species
-#' @param mu.alpha - mean for alpha parameter: default is 0
-#' @param sd.alpha - sd for alpha parameter: default is 1
-#' @param mu.beta - mean for beta parameter: default is 0
-#' @param sd.beta - sd for beta parameter: default is 1
-#' @return mat - matrix representation of abundance
-#' @return vec - vectorized representation of abundance
-#' @return alpha.true - true values of alpha
-#' @return beta.true - true values of alpha
-#' @return theta.true - true values of alpha
-#' @return z.true - true values of alpha
+#' @param coord1 - mcmc object with first coordinate of projection
+#' @param coord2 - mcmc object with first coordinate of projection
+#' @param type - plot type, options are 'points', 'circles', 'scatter'
+#' @param items - items to display uncertainty, only works for circles or scatter
+#' @return plot.obj - ggplot object
 #' @export
 
-CredibleViz <- function(n, p, mu.alpha = 0, sd.alpha = 1 , mu.beta = 0, sd.beta = 1){
-  alpha.true <- stats::rnorm(n, mu.alpha, sd.alpha)
-  beta.true <- stats::rnorm(p, mu.alpha, sd.alpha)
-  z.true <- matrix(stats::rnorm(n*2),n,2)
-  theta.true <- matrix(stats::rnorm(p*2), nrow=p, ncol=2)
-  theta.true[1,2] <- 0
-  while(theta.true[1,1] < 0){
-    theta.true[1,1] <- stats::rnorm(1)
+CredibleViz <- function(coord1, coord2, type = 'points', item = NULL){
+  ### Create thin data
+  num.pts <- ncol(coord1)
+
+  coord1.thin <- coord1 %>% as.data.frame() %>% gather %>% dplyr::mutate(key = ordered(key, levels=unique(key)))
+  coord2.thin <- coord2 %>% as.data.frame() %>% gather %>% dplyr::mutate(key = ordered(key, levels=unique(key)))
+  combined <- data.frame(lv1 = coord1.thin$value, lv2 = coord2.thin$value, key = coord1.thin$key)
+  combined.summarized <- combined %>% dplyr::group_by(key) %>% dplyr::summarise(mean.lv1 = mean(lv1), mean.lv2 = mean(lv2)) %>% dplyr::mutate(id = 1:num.pts)
+
+  ### Points plot
+  plot.obj <- ggplot2::ggplot(combined.summarized, aes(x=mean.lv1, y=mean.lv2)) + ggplot2::geom_text(aes(label=id, color=as.factor(id))) + ggplot2::guides(color=FALSE) + ggplot2::xlab('latent variable one') + ggplot2::ylab('latent variable two')  + ggplot2::theme_bw() + ggplot2::ggtitle('')
+
+  gg_color_hue <- function(n) {
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
   }
-  while(theta.true[2,1] < 0){
-    theta.true[2,1] <- stats::rnorm(1)
+  cols = gg_color_hue(num.pts)
+
+  if (type == 'scatter'){
+    ### Scatter plot
+    # match colors
+
+    for (item.numb in 1:length(items)){
+      sims.tmp <- combined %>% dplyr::filter(key == items[item.numb])
+      item.pos <- (1:num.pts)[colnames(coord1) == items[item.numb]]
+      plot.obj <- plot.obj + ggplot2::geom_point(data = sims.tmp, aes(x=lv1, y=lv2), alpha=.15, color = cols[item.pos])
+      plot.obj
+    }
+    plot.obj <- plot.obj + ggplot2::geom_text(aes(label=id, color=as.factor(id))) + ggplot2::ylim(-3.1,3.1) + ggplot2::xlim(-3.1,3.1)
+    plot.obj
+  } else if (type == 'circles'){
+
+    ### circle plot
+    all.itempos <- (1:num.pts)[colnames(coord1) %in% items]
+
+    for (item.numb in 1:length(items)){
+      sims.temp <- combined %>% dplyr::filter(key == items[item.numb]) %>% dplyr::select(lv1,lv2) %>% as.matrix
+      fit <- MASS::cov.mve(sims.temp, quantile.used = nrow(sims.temp) * 0.95, nsamp = 100)
+      points_in_ellipse <- sims.temp[fit$best, ]
+      ellipse_boundary <- predict(cluster::ellipsoidhull(points_in_ellipse))
+      ellipse_boundary_df <- data.frame(ellipse_boundary)
+      plot.obj <- plot.obj + ggplot2::geom_path(data=ellipse_boundary_df, aes(x=V1, y=y),linetype=3, color=cols[all.itempos[item.numb]])
+    }
+
+    plot.obj <- plot.obj + ggplot2::ylim(-3.1,3.1) + ggplot2::xlim(-3.1,3.1)
   }
 
-  Z.theta <- z.true %*% t(theta.true)
-  mu.true <- rep(alpha.true, each = p) + rep(beta.true, n) + as.numeric(t(Z.theta))
-  mu.matrix <- matrix(mu.true, n, p, byrow=T)
-  Y <- stats::rpois(n*p,exp(mu.true))
-  Y.matrix <- matrix(Y, n, p , byrow=T)
-  return(list(mat = Y.matrix, vec = Y, alpha.true = alpha.true, beta.true = beta.true, theta.true = theta.true, z.true = z.true))
+  return(list(plot.obj = plot.obj))
 }
+
